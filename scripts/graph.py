70042769aa29b9f87e582628b2bbafde366e0469
Michael Goppert <mi@gopp.dev>
2025-11-27 22:48:09 -0600

    Modify graphing parameters

--- scripts/graph.py
+++ scripts/graph.py
@@ -6,6 +6,7 @@ Author: Will Bolduc <wbolduc@cs.utexas.edu>
 """
 import json
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 import subprocess
 from dataclasses import dataclass, field
@@ -49,40 +50,37 @@ class GraphTask:
         me2 += self.workload + "-" + self.stage + "-" + self.metric
         return me2
 
+    def axes(self):
+        x = "number of threads"
+        units = ""
+        if self.metric == "runtime":
+            units = "sec"
+        elif self.metric == "operations":
+            units = "mops"
+        else:
+            units = "mops/sec"
+        y = f"{self.stage} - {self.metric} ({units})"
+        return (x, y)
+
+    def title(self):
+        if self.workload == "a":
+            return "50% Reads, 50% Updates"
+        elif self.workload == "b":
+            return "95% Reads, 5% Updates"
+        elif self.workload == "c":
+            return "100% Reads"
+        elif self.workload == "d":
+            return "95% Reads, 5% Inserts"
+        elif self.workload == "e":
+            return "95% Scans, 5% Inserts"
+        elif self.workload == "f":
+            return "50% Reads, 50% RMW"
+        else:
+            raise NotImplementedError
+
 def read_all():
     with open("../filtered_data.json", "r") as f:
         return json.load(f)
-
-def axis_names(task):
-    x = "number of threads"
-    stage = task.stage
-    metric = task.metric
-    units = ""
-    if metric == "runtime":
-        units = "sec"
-    elif metric == "operations":
-        units = "op"
-    else:
-        units = "op/sec"
-    y = f"{stage}: {metric} ({units})"
-    return (x, y)
-
-def title(task):
-    if task.workload == "a":
-        return "50% Reads, 50% Updates"
-    elif task.workload == "b":
-        return "95% Reads, 5% Updates"
-    elif task.workload == "c":
-        return "100% Reads"
-    elif task.workload == "d":
-        return "95% Reads, 5% Inserts"
-    elif task.workload == "e":
-        return "95% Scans, 5% Inserts"
-    elif task.workload == "f":
-        return "50% Reads, 50% RMW"
-    else:
-        # XXX: O_O
-        return "WAT"
 
 def extract_from(task, data):
     # Should all have data for same set of thread counts. (?)
@@ -92,6 +90,8 @@ def extract_from(task, data):
         for db in task.dbs:
             base[db]["mean"][i-1] = data[db][task.workload][str(i)][task.stage][task.metric]["mean"]
             base[db]["std"][i-1] = data[db][task.workload][str(i)][task.stage][task.metric]["std"]
+            if task.metric == "throughput":
+                base[db]["mean"][i-1] /= 1000000
     return (x, base)
 
 def graph_one(cfg, task, data):
@@ -103,11 +103,12 @@ def graph_one(cfg, task, data):
         std = y_map[db]["std"]
         plt.errorbar(x, y, yerr=std, fmt=cfg.markers[db], linestyle=cfg.linestyles[db], color=cfg.colors[db], capsize=4, label=db.split("_db")[0])
 
-    x_axis, y_axis = axis_names(task)
-    tit = title(task)
+    x_axis, y_axis = task.axes()
+    tit = task.title()
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
     plt.title(tit)
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
     plt.subplots_adjust(right=0.75)
     plt.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), frameon=False)
 
